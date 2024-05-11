1474e79bbe7784cb161fda0e19aab38158072f7e
Shrabya35 <shrabyaraj@gmail.com>
2024-05-11 18:24:43 +0545

    scheduled events added

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,76 +1,91 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../layout/Layout";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Layout from "../layout/Layout";
 
-function Events() {
+const Events = () => {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const API_KEY =
-        "d138de469e9d65b4b145f86b2d724fb0d75100e86e4863a5190484bf1c82c80c";
-      const league_id = 152;
-      const from = "2024-05-10";
-      const to = "2024-05-10";
+    const fetchEvents = async () => {
+      const options = {
+        method: "GET",
+        url: "https://sofascore.p.rapidapi.com/tournaments/get-scheduled-events",
+        params: { categoryId: "1" },
+        headers: {
+          "X-RapidAPI-Key":
+            "c72c804f5bmsh5f8cd831b7aa9a8p1fc11ejsn9d8306a795a3",
+          "X-RapidAPI-Host": "sofascore.p.rapidapi.com",
+        },
+      };
 
       try {
-        const response = await axios.get(
-          `https://apiv3.apifootball.com/?action=get_events&from=${from}&to=${to}&league_id=${league_id}&APIkey=${API_KEY}`
-        );
-
-        console.log("Response status:", response.status);
-        console.log("Response data:", response.data);
-
-        if (response.status !== 200) {
-          throw new Error(`Failed to fetch data: ${response.statusText}`);
-        }
-
-        const data = response.data;
-
-        if (data.error) {
-          throw new Error(`API Error: ${data.error}`);
-        }
-
-        setEvents(data);
+        const response = await axios.request(options);
+        console.log(response.data); // Check the structure of response.data
+        setEvents(response.data.events); // Assuming events are under a property named 'events'
         setLoading(false);
       } catch (error) {
-        console.error("Error fetching data:", error.message);
-        setError("Error fetching data. Please try again later.");
+        console.error(error);
+        setError("Failed to fetch events.");
         setLoading(false);
       }
     };
 
-    fetchData();
+    fetchEvents();
   }, []);
+
+  console.log(events);
 
   return (
     <Layout>
-      <div>
-        {loading && <p className="loading">Loading...</p>}
-        {error && <p className="loading">{error}</p>}
-        {events && (
-          <div className="events-container">
-            <h1>Events</h1>
-            <ul>
-              {events.map((event) => (
-                <li key={event.match_id}>
-                  <div>Date: {event.match_date}</div>
-                  <div>Home Team: {event.match_hometeam_name}</div>
-                  <div>Away Team: {event.match_awayteam_name}</div>
-                  <div>
-                    Final Score: {event.match_hometeam_score} -
-                    {event.match_awayteam_score}
+      <div className="Events">
+        <div className="events-container">
+          <h1 className="scheduled-events">Scheduled Matches</h1>
+          {loading && <p className="loading">Loading...</p>}
+          {error && <p className="loading">Error: {error}</p>}
+          <div className="events-section">
+            {events
+              .filter((match) => match.tournament.slug === "premier-league")
+              .map((event) => (
+                <div className="event-card" key={event.id}>
+                  <div className="event-card-top">
+                    <div className="event-round">
+                      EPL R{event.roundInfo.round}
+                    </div>
+                    <div className="event-status">
+                      {event.status.description}
+                    </div>
                   </div>
-                </li>
+                  <div className="event-card-bottom">
+                    <div className="ec-bottom-left">
+                      <div className="events-teamname">
+                        {event.homeTeam.name}
+                      </div>
+                      <div className="events-teamname">
+                        {event.awayTeam.name}
+                      </div>
+                    </div>
+                    <div className="ec-bottom-right">
+                      <div className="event-time-text">Today</div>
+                      <div className="event-time">
+                        {new Date(
+                          event.startTimestamp * 1000
+                        ).toLocaleTimeString([], {
+                          hour: "numeric",
+                          minute: "2-digit",
+                          hour12: true,
+                        })}{" "}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               ))}
-            </ul>
           </div>
-        )}
+        </div>
       </div>
     </Layout>
   );
-}
+};
 
 export default Events;
